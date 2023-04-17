47ce482a58603063ae644362d3bd464e89201345
Ricardo Salambi <ricardox200@gmail.com>
2023-04-17 21:36:14 +0200

    Frontend components for the capturing of user data

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,28 @@ import './App.css';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <div>
-      This is a Test
-    </div>
+    <form>
+            
+        <div className="fields">
+
+            <div className="header"> <h2>Proficieny Test</h2> </div>
+
+            <div><input type="text" name="name" placeholder="Name" value=""></input></div>
+            
+            <div><input type="text" name="surname" placeholder="Surname" value=""></input></div>
+
+            <div><input type="number" name="id" placeholder="ID Number" value=""></input></div>
+
+            <div><input type="text" name="dob" placeholder="Date of Birth" value=""></input></div>
+
+            <div><button type="submit" name="submit"> Submit </button></div>
+
+            <div><button name="cancel"> Cancel </button></div>
+
+        </div>
+        
+
+    </form>
   );
 }
 
